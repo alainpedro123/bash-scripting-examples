7e4e4236f2148d1dfd9656e4a14c832642d088bb
Alain Afonso <48403528+alainpedro123@users.noreply.github.com>
2022-01-22 11:46:37 +0100

    Update 1-variables.sh

--- 1-variables.sh
+++ 1-variables.sh
@@ -8,3 +8,32 @@ echo "My name is $NAME"
 
 read -p "Enter your name: " NAME
 echo "Hello $NAME, nice to meet you!"
+
+
+# 2. ENVIRONMENT VARIABLES
+
+############
+# PATH - the whole path until the current directory
+# USER - The username
+# HOME - Default path to the user’s home directory
+# EDITOR - Path to the program which edits the content of files
+# UID - User’s unique ID
+# TERM - Default terminal emulator
+# SHELL - Shell being used by the user
+############
+
+# Accessing the Variables Values
+echo $VARIABLE
+echo $PATH
+
+
+# Set New Environment Variables
+VARIABLE_NAME=variable_value
+
+
+# Deleting Variables
+unset VARIABLE_NAME
+
+
+# To set value of an environment variable
+export VARIABLE=VALUE
